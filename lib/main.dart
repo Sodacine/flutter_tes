d473766c46dc5ac4f24a021c1707d6863226ef9b
Sodacine <rkencana110@gmail.com>
2026-04-16 17:40:00 +0700

    Pertemuan 5

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tes/page/dashboard.dart';
+import 'package:flutter_tes/page/profile_page.dart';
 import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 
 void main() {
@@ -6,75 +8,40 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
-  static final title = 'Rangga Satria Kencana';
-
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  final List<Widget> _page = [
-    Container(color: Colors.red),
-    Container(color: Colors.blueAccent),
-    Container(color: Colors.blueGrey),
-    Container(color: Colors.grey),
-    Container(color: Colors.greenAccent),
-  ];
+  final List<Widget> _pages = [DashboardPage(), ProfilePage()];
 
-  var _currentIndex = 0;
+  var currentPage = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: MyApp.title,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text(MyApp.title),
-          backgroundColor: Colors.blue,
-        ),
-        body: _page[_currentIndex],
+        body: _pages[currentPage],
         bottomNavigationBar: SalomonBottomBar(
-          currentIndex: _currentIndex,
-          onTap: (i) => setState(() => _currentIndex = i),
+          currentIndex: currentPage,
+          onTap: (i) => setState(() => currentPage = i),
           items: [
-            /// Beranda
+            // Beranda
             SalomonBottomBarItem(
               icon: Icon(Icons.home),
               title: Text('Beranda'),
-              selectedColor: Colors.red,
+              selectedColor: Colors.blue,
             ),
-
-            /// Kalendar
-            SalomonBottomBarItem(
-              icon: Icon(Icons.calendar_month),
-              title: Text('Kalendar'),
-              selectedColor: Colors.blueAccent,
-            ),
-
-            /// Maps
-            SalomonBottomBarItem(
-              icon: Icon(Icons.approval_rounded),
-              title: Text('Maps'),
-              selectedColor: Colors.blueGrey,
-            ),
-
-            /// Search
-            SalomonBottomBarItem(
-              icon: Icon(Icons.search),
-              title: Text('Search'),
-              selectedColor: Colors.grey,
-            ),
-
-            /// Profile
+            // Profile
             SalomonBottomBarItem(
               icon: Icon(Icons.person),
               title: Text('Profile'),
-              selectedColor: Colors.greenAccent,
+              selectedColor: Colors.blue,
             ),
           ],
         ),
